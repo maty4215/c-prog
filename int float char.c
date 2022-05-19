455095c4d4f53419486ca9efc5f375e496f812ed
maty4215 <96356426+maty4215@users.noreply.github.com>
2022-05-19 17:19:22 +0200

    Add files via upload

---
+++ int float char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int věk = 16;;
+    float prumer = 3.15;
+    char jmeno[] = "Matěj";
+    char prijmeni[] = "Fárek";
+
+        printf("Dobrý den, jmenuji se %s %s,\nje mi %d let,\na chtěl bych si opravit průměr %f\n",jmeno,prijmeni,věk,prumer);
+    
+    return 0;
+}
